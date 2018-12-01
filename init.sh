49d403a3725014e6e4d0a3e3210b279e30e98ccc
Amir Haghighati <haghighati.amir@gmail.com>
2018-12-01 17:24:37 -0500

    update init script

--- init.sh
+++ init.sh
@@ -3,9 +3,7 @@
 ls /dev/video*
 if [ $? -eq 0 ]; then
     echo "Video device(s) mounted successfully!"
-    echo "Starting to stream the video..."
-    ffmpeg -re -i v1.mp4 -map 0:v -f v4l2 /dev/video0
-    echo "Video stream ended."
+    stream
 else
-    echo "No mounted video device founded"
+    echo "No mounted video device founded."
 fi
