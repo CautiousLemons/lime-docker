2b5b62561f07be50e962cf238d5e918da691cc4e
Amir Haghighati <haghighati.amir@gmail.com>
2018-12-03 12:10:26 -0500

    update init script so that it runs fire fox upon creation and streams the media

--- init.sh
+++ init.sh
@@ -2,8 +2,11 @@
 # Start streaming the file
 ls /dev/video*
 if [ $? -eq 0 ]; then
+    echo "Changing permissions..."
+    lime
     echo "Video device(s) mounted successfully!"
-    stream
+    stream &
+    firefox -new-tab meet.jit.si/random
 else
     echo "No mounted video device founded."
 fi
