ca37504355c50abb90855cea718bf04b8240c0d8
Amir Haghighati <haghighati.amir@gmail.com>
2018-12-10 19:22:08 -0500

    update init script

--- init.sh
+++ init.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # Start streaming the file
+xvfb-run firefox &
+sleep 2
+ps -aux | grep Xvfb | kill `awk '{print $2}'i`
 ls /dev/video*
 if [ $? -eq 0 ]; then
     echo "Changing permissions..."
     lime
     echo "Video device(s) mounted successfully!"
     stream &
-    firefox -new-tab meet.jit.si/random
+    cp user.js .mozilla/firefox/*.default/
+    xvfb-run firefox --new-tab meet.jit.si/random
 else
     echo "No mounted video device founded."
 fi
