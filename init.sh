f0d1115c23dfe0f091c3b585ced8779c41947024
Amir Haghighati <haghighati.amir@gmail.com>
2018-12-19 18:10:41 -0500

    add compose file

--- init.sh
+++ init.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Start streaming the file
-xvfb-run firefox &
-sleep 2
-ps -aux | grep Xvfb | kill `awk '{print $2}'i`
+#xvfb-run firefox &
+#sleep 2
+#ps -aux | grep Xvfb | kill `awk '{print $2}'i`
 ls /dev/video*
 if [ $? -eq 0 ]; then
     echo "Changing permissions..."
     lime
     echo "Video device(s) mounted successfully!"
     stream &
-    cp user.js .mozilla/firefox/*.default/
+#    cp user.js .mozilla/firefox/*.default/
     echo "\n\n=================================== COPIED =========================\n\n"
-    xvfb-run firefox --new-tab meet.jit.si/random
+    firefox --new-tab meet.jit.si/random
 else
     echo "No mounted video device founded."
 fi
